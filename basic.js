76562ebcc8a8cea512065556707acee991aba65a
Kathrin <kathrin@kathrins-air.codefactoryvienna.local>
2018-04-24 15:30:06 +0200

    newupdates

--- basic.js
+++ basic.js
@@ -25,3 +25,48 @@ function highvalue() {
 
 
 /*Basic Exercise 3 */
+
+
+var x = 0;
+var array = Array();
+
+function add_element_to_array()
+{
+ array[x] = document.getElementById("text1").value;
+ alert("Element: " + array[x] + " Added at index " + x);
+ x++;
+ document.getElementById("text1").value = "";
+}
+
+
+function display_array()
+{
+   var p = 1;   
+    
+   for (var y=0; y<array.length; y++)
+   {
+     p *= array [y];
+   }
+   document.getElementById("Result").innerHTML = p;
+}
+
+
+/*Basic Exercise 4 */
+
+
+function uppercase(str) {
+
+var str = "the perfect problem solver"; 
+var array1 = str.split(" ");
+var newarray1 = []; 
+
+for (var i = 0; i < array1.length; i++) {
+	newarray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));	
+}
+
+return newarray1.join(" "); 
+
+}
+
+console.log(uppercase());
+
