c41059c64c5e869984b09301740bbcf3b973d356
Kathrin <kathrin@Kathrins-MacBook-Air.local>
2018-04-24 22:38:18 +0200

    update intermediatefiles

--- intermediate.js
+++ intermediate.js
@@ -1,37 +1,29 @@
+
 var students = [
-	{ name: "Martin", 
-	  points: 76  
-	},
-{
-	name: "Thomas", 
-	points: 85 
-},
+['Martin', 76], 
+['Thomas', 85], 
+['Klaus', 65], 
+['Maria', 93], 
+['David', 81]];
 
-{
-	name: "Klaus",
-	points: 65 
-},
+var averagemarks = 0;
 
-{
-	name: "Maria", 
-	points: 93
-},
+for (i = 0; i < students.length; i++) {
 
-{
-	name: "David",
-	points: 81
-}
+    averagemarks += students [i][1];
+    var avg = (averagemarks / students.length); 
+    }
 
-]; 
+    alert("Average grade:" + (averagemarks) / students.length);
 
-sum = 0; 
-
-for (var i=0; i< students.length; i++) {
-
-	sum = sum + students.points[i];  
-	return sum; 
-}
-
-result = sum / students.length; 
-
-console.log(result); 
+    if (avg < 60) {
+        console.log("Grade : F");
+    } else if (avg < 70) {
+        console.log("Grade : D");
+    } else if (avg < 80) {
+        console.log("Grade : C");
+    } else if (avg < 90) {
+        console.log("Grade : B");
+    } else if (avg < 100) {
+        console.log("Grade : A");
+    }
